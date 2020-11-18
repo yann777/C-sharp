06557ee428c22c808b5ab14269018cd22351b5d5
Yan Oliveira Ferreira <yan12oliveira@gmail.com>
2020-11-18 16:18:59 -0300

    Tabuada finalizada

---
+++ Tabuada/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio_Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual Tabuada você gostaria de saber?");
+            int tabuada = int.Parse(Console.ReadLine());
+            int contador = 0;
+            Console.WriteLine("-------------------------------------");
+
+            while(contador <= 10){
+                Console.WriteLine(tabuada * contador);
+                contador = contador + 1;
+            }
+        }
+    }
+}
---
+++ Tabuada/README.md
@@ -0,0 +1,2 @@
+# Eercicio_Tabuada
+Exercício de Tabuada até o 10
